0bb9f184f7b0abb0c761acd16cf03de20b80cd10
oliviadp <olivia.osypovych@gmail.com>
2024-11-01 16:33:01 +0200

    fin

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,17 @@
-import imgName from "../main.js"
-const API_KEY = "46809493-025d46e6636e036ef2e71f554";
+export default fetchData;
+
 const BASE_URL = "https://pixabay.com/api/";
+const params = new URLSearchParams ({
+    key: "46809493-025d46e6636e036ef2e71f554",
+    q: "",
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true
 
-
+});
 
 function fetchData (imgName) {
-    const params = {
-        q,
-        image_type: photo,
-        orientation: horizontal,
-        safesearch: true
-        };
+    params.set("q", imgName);
    return fetch(`${BASE_URL}?${params}`)
         .then(response => {
             if(!response.ok) {
@@ -18,5 +19,6 @@ function fetchData (imgName) {
             }
 
             return response.json();
+              
         })
     }
